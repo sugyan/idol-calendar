5fe6c52fa58e28ec14d7b8920f7453de6d5d2c36
sugyan <sugi1982@gmail.com>
2013-02-21 00:50:48 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@
 
 Calendar.find_or_create(:cid => 'jetlyzer@yahoo.co.jp')
 Calendar.find_or_create(:cid => 'jhqrtl9na3gev1vihqnpt3qvgo@group.calendar.google.com')
+Calendar.find_or_create(:cid => 'tsukubatvevent@gmail.com')
