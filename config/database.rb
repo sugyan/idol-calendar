3006cefe130ec67466e04b2a5c1953dc7d9fc5f5
sugyan <sugi1982@gmail.com>
2013-02-20 17:55:11 +0900

    initial commit

---
+++ config/database.rb
@@ -0,0 +1,7 @@
+Sequel::Model.plugin(:schema)
+Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+Sequel::Model.db = case Padrino.env
+  when :development then Sequel.connect("postgres://localhost/idol_calendar_development", :loggers => [logger])
+  when :production  then Sequel.connect("postgres://localhost/idol_calendar_production",  :loggers => [logger])
+  when :test        then Sequel.connect("postgres://localhost/idol_calendar_test",        :loggers => [logger])
+end
