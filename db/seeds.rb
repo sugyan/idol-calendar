224b5b29bb2995965fc2246a84e0e3833e58696b
sugyan <sugi1982@gmail.com>
2013-02-21 17:30:00 +0900

    add calendar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 
-Calendar.find_or_create(:cid => 'jetlyzer@yahoo.co.jp')
-Calendar.find_or_create(:cid => 'jhqrtl9na3gev1vihqnpt3qvgo@group.calendar.google.com')
-Calendar.find_or_create(:cid => 'tsukubatvevent@gmail.com')
-Calendar.find_or_create(:cid => 'sei@nomake.jp')
+Calendar.find_or_create(:cid => 'jetlyzer@yahoo.co.jp')                                 # 無銭カレンダー
+Calendar.find_or_create(:cid => 'jhqrtl9na3gev1vihqnpt3qvgo@group.calendar.google.com') # ライブアイドルカレンダー
+Calendar.find_or_create(:cid => 'tsukubatvevent@gmail.com')                             # PigooLIVE
+Calendar.find_or_create(:cid => 'bbvrqpohqa6k9mib9788pmk3ro@group.calendar.google.com') # 北海道アイドルカレンダー
+Calendar.find_or_create(:cid => 'i90cg2r2eot24hqtosvj97pdi8@group.calendar.google.com') # 北海道アイドルカレンダー（道外イベント）
+Calendar.find_or_create(:cid => 'siig2006@gmail.com')                                   # Sendai Idol Inteligence Guide
+Calendar.find_or_create(:cid => 'j76ss5h9b2gu1hj3bn56r5jkec@group.calendar.google.com') # NAGOYA 有銭
+Calendar.find_or_create(:cid => 'pdne7f9gcavkl92dqoqv8ubou0@group.calendar.google.com') # NAGOYA 無銭
+Calendar.find_or_create(:cid => '9p0ilmeposq8gsnch5516l3qbg@group.calendar.google.com') # NAGOYA 購入特典
+Calendar.find_or_create(:cid => 'ipqrp3ma3m6trcb0vdlnmts6s0@group.calendar.google.com') # 新潟アイドル・ゆるきゃらイベント
+Calendar.find_or_create(:cid => 'musenkansai@yahoo.co.jp')                              # 無銭カレンダー関西版
+Calendar.find_or_create(:cid => 'yuukicalendar@gmail.com')                              # フリーライブin関西
+Calendar.find_or_create(:cid => 'wa.cclive@gmail.com')                                  # 和歌山＜ライヴ＞情報
+Calendar.find_or_create(:cid => 'michio.hatakeyama@gmail.com')                          # 広島アイドルカレンダー(仮)
+Calendar.find_or_create(:cid => 'kagoshima.idol.schedule@gmail.com')                    # 鹿児島アイドルカレンダー
+Calendar.find_or_create(:cid => 'sei@nomake.jp')                                        # 青SHUN学園
