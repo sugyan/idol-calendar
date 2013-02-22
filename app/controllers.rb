ee3d034250a2ab61c4e279d5e1f51e30d251de84
sugyan <sugi1982@gmail.com>
2013-02-23 03:01:28 +0900

    update template

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,6 +1,6 @@
 IdolCalendar.controllers  do
   get :index do
-    @events = Event.filter{ start >= Date.today }.order(:start).paginate(params[:page].to_i.nonzero? || 1, 200)
+    @events = Event.filter{ start >= Date.today }.order(:start, :end).paginate(params[:page].to_i.nonzero? || 1, 200)
     render 'index'
   end
 
