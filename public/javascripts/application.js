88c92bf5cba1a2c8dbdba19ad353d50d4f7e5a05
sugyan <sugi1982@gmail.com>
2013-02-24 00:41:24 +0900

    add permalink for search page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,7 @@ $(function () {
     if (hr.get(0)) {
         $('html, body').animate({ scrollTop: hr.siblings().eq(hr.index() - 1).offset().top - 40 }, 1500);
     }
+    $('#permalink').click(function () {
+        this.select();
+    });
 });
